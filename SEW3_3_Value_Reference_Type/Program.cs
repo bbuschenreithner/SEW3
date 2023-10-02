5caedfda6e6e5180f4824a8cb8071c4ca3dcbf82
Benjamin Buschenreithner <b.buschenreithner@josephinum.at>
2023-10-02 09:29:03 +0200

    Projektdateien hinzufügen.

---
+++ SEW3_3_Value_Reference_Type/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SEW3_3_Value_Reference_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
