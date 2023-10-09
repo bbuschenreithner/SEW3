d4e0747815cbb8215a7717f91cc4f34f466d2fb7
Benjamin Buschenreithner <b.buschenreithner@josephinum.at>
2023-10-09 10:10:40 +0200

    Namespaces

---
+++ SEW3_04_Namespaces/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FJ.IT;
+
+Student max = new Student();        // Namenskonflikt ohne using FJ.IT
+FJ.Agriculture.Student ben = new FJ.Agriculture.Student();
---
+++ SEW3_04_Namespaces/StudentIT.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FJ.IT;
+public class Student
+{
+    public string Name { get; set; }
+    public string ProgrammingSkills { get; set; }
+}
